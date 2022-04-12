cbecd69ab51c26df1dbc5a90021d7721a91e95a7
boyanbenev <boyan.benev@gmail.com>
2022-04-12 08:20:43 +0200

    Refactor assessment.py and utils.py

--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@
 
 def sorted_desc_by_transaction(df, number):
     '''Takes data frame sorting desc by transctions limited to NUMBER (second argument)'''
-    sorted_by_transactions = df.sort_values(by=['Transactions'], ascending=False)
-    return sorted_by_transactions.head(number)
+    df_sorted_by_transactions = df.sort_values(by=['Transactions'], ascending=False)
+    return df_sorted_by_transactions.head(number)
